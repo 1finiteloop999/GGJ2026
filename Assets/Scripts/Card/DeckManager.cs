6ae4c5c648bd9a044c8ab27729c237f2d9ee2fb2
1finiteloop999 <graaaw@163.com>
2026-01-31 22:00:19 +0000

    025_添加Sell和Use区域UI和音效接口

--- Assets/Scripts/Card/DeckManager.cs
+++ Assets/Scripts/Card/DeckManager.cs
@@ -30,6 +30,12 @@ public class DeckManager : MonoBehaviour
     [SerializeField] private RectTransform sellArea;
     [SerializeField] private RectTransform useArea;
 
+    [Header("区域UI（可选）")]
+    [Tooltip("出售区域UI组件")]
+    [SerializeField] private DropAreaUI sellAreaUI;
+    [Tooltip("使用区域UI组件")]
+    [SerializeField] private DropAreaUI useAreaUI;
+
     [Header("初始牌库")]
     [SerializeField] private List<CardData> initialCards = new List<CardData>();
 
@@ -528,6 +534,9 @@ public class DeckManager : MonoBehaviour
         handCards.Remove(card);
         Destroy(card.gameObject);
 
+        // 播放成功音效并重置区域状态
+        sellAreaUI?.OnOperationSuccess();
+
         OnCardSold?.Invoke(card);
     }
 
@@ -552,6 +561,9 @@ public class DeckManager : MonoBehaviour
 
         handCards.Remove(card);
         Destroy(card.gameObject);
+
+        // 播放成功音效并重置区域状态
+        useAreaUI?.OnOperationSuccess();
 
         OnCardUsed?.Invoke(card);
     }
